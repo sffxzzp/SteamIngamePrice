8232aef263bb1e59250c58661b45350517403792
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2022-06-02 08:02:14 +0000

    完成首页 | todo: 详情（或可用弹窗弹出，减少工作量）

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"io"
 	"net/http"
+	"os"
 )
-
-type ()
 
 func httpGet(url string) ([]byte, bool) {
 	resp, err := http.Get(url)
@@ -22,13 +21,40 @@ func httpGet(url string) ([]byte, bool) {
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// todo
+	appid := "730"
+	apikey := os.Getenv("apikey")
+	// apikey := "<apikey>"
+	mod := r.URL.Query().Get("mod")
+	var data []byte
+	var err bool
+	if mod == "schema" {
+		data, err = httpGet("https://api.steampowered.com/IEconItems_" + appid + "/GetSchema/v2/?language=zh-CN&key=" + apikey)
+	} else if mod == "price" {
+		data, err = httpGet("https://api.steampowered.com/ISteamEconomy/GetAssetPrices/v1/?language=zh-CN&appid=" + appid + "&key=" + apikey)
+	} else {
+		w.WriteHeader(400)
+		w.Write([]byte("Invalid mod"))
+		return
+	}
+	if err {
+		w.WriteHeader(500)
+		w.Write([]byte("Network error"))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Cache-Control", "public, max-age=14400, s-maxage=14400, stale-while-revalidate=14400")
+	w.Write(data)
 }
 
 func main() {
-	http.HandleFunc("/", Handler)
+	println("Listening on http://localhost:8080")
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+	http.HandleFunc("/api/", Handler)
 	http.ListenAndServe("127.0.0.1:8080", nil)
 }
 
-// https://api.steampowered.com/IEconItems_730/GetSchema/v0002/?key=<apikey>
-// https://api.steampowered.com/ISteamEconomy/GetAssetPrices/v0001/?appid=730&class_name0=def_index&class_value0=4842&key=<apikey>
+// http://127.0.0.1:8080/
+
+// https://api.steampowered.com/IEconItems_730/GetSchema/v2/?key=<apikey>
+// https://api.steampowered.com/ISteamEconomy/GetAssetPrices/v1/?appid=730&key=<apikey>
