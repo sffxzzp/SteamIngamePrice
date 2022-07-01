89ed195768be4f2eb4d187ab11398f7a8fe952b3
sffxzzp <sffxzzp@126.com>
2022-07-01 13:54:54 +0800

    增加英语

--- dist/api/index.go
+++ dist/api/index.go
@@ -24,12 +24,16 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	appid := "730"
 	apikey := os.Getenv("apikey")
 	mod := r.URL.Query().Get("mod")
+	lang := r.URL.Query().Get("lang")
+	if lang != "zh-CN" {
+		lang = "en-US"
+	}
 	var data []byte
 	var err bool
 	if mod == "schema" {
-		data, err = httpGet("https://api.steampowered.com/IEconItems_" + appid + "/GetSchema/v2/?language=zh-CN&key=" + apikey)
+		data, err = httpGet("https://api.steampowered.com/IEconItems_" + appid + "/GetSchema/v2/?language=" + lang + "&key=" + apikey)
 	} else if mod == "price" {
-		data, err = httpGet("https://api.steampowered.com/ISteamEconomy/GetAssetPrices/v1/?language=zh-CN&appid=" + appid + "&key=" + apikey)
+		data, err = httpGet("https://api.steampowered.com/ISteamEconomy/GetAssetPrices/v1/?language=" + lang + "&appid=" + appid + "&key=" + apikey)
 	} else {
 		w.WriteHeader(400)
 		w.Write([]byte("Invalid mod"))
